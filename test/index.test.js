0d5097c7b068ef076117a50f5883dffcf01d431d
Parker Helland <hellandp@gmail.com>
2019-04-12 15:39:43 -0700

    just playing around with sort and join

--- test/index.test.js
+++ test/index.test.js
@@ -2,10 +2,16 @@ const doIt = require('../lib/index');
 
 describe('FILTER TEST', () => {
 
-  it('returns numbers > 5 into string: Wow, ${number} is big!', () => {
+  it('returns numbers > 5 into string Wow, ${number} is big!', () => {
     const arr = [1, 3, 5, 7, 2];
     const result = doIt(arr);
     expect(result).toEqual(['Wow, 7 is big!']);
   });
 
+  it('sort & stringify', () => {
+    const arr = [1, 3, 5, 7, 2, 6];
+    const result = doIt(arr);
+    expect(result.join(' ')).toEqual('Wow, 6 is big! Wow, 7 is big!');
+  });
+  
 });
