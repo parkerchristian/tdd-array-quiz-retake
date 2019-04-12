c3ae2fb5533d53470f5e60b1f30a4cf38847d01d
Parker Helland <hellandp@gmail.com>
2019-04-12 15:20:06 -0700

    mapped filtered numbers in function, test passed

--- test/index.test.js
+++ test/index.test.js
@@ -5,7 +5,7 @@ describe('FILTER', () => {
   it('filter array', () => {
     const arr = [1, 3, 5, 7, 2];
     const result = doit(arr);
-    expect(result).toEqual([7]);
+    expect(result).toEqual(['Wow, 7 is big!']);
   });
-  
+
 });
